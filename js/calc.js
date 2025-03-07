18956dba7d63e91afe5a7f6ba47592df2e9303ff
Ayushman Mishra <mishra.ayushman.krisg@gmail.com>
2025-03-07 15:58:45 +0530

    Updated HTML and JS for calculator

--- js/calc.js
+++ js/calc.js
@@ -13,13 +13,8 @@ function calculate() {
         
         case "/": finalValue = divide(num1, num2); break;
 
-        case "^": finalValue = exponention(num1, num2); break;
+        case "^": finalValue = exponiation(num1, num2); break;
 
-        case "√": finalValue = squareRoot(num1); break;
-
-        case "%": finalValue = percantage(num1, num2); break;
-
-        case "!": finalValue = factorial(num1); break;
     }
     printFinalValue(finalValue);
 
@@ -45,26 +40,7 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
-function exponention(num1, num2) {
-    if (num2 === 0) return 1;
-    let pow = 1;
-    for (let i=0; i < num2; i++) {
-        pow = multiply(pow,num1)
-    }
-    return pow;
+function exponiation(num1, mum2) {
+    return num1 ** num2;
 }
 
-function squareRoot(num1) {
-    return Math.sqrt(num1)
-}
-function percantage(num1, num2) {
-    return divide(multiply(num1, num2),100) ;    
-}
-function factorial(num1) {
-   if (num1 === 1 || num1 === 0) return 1;
-   let factori = 1;
-   for (let a=2; a <= num1; a++) {
-        factori = multiply(factori, a)
-   }
-   return factori
-}
